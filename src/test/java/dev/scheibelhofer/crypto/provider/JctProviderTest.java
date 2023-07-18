5d383846f705fac90659f51a956e9883e62bbc3d
KarlScheibelhofer <karl.scheibelhofer.75@gmail.com>
2023-07-18 21:30:23 +0200

    add provider test

---
+++ src/test/java/dev/scheibelhofer/crypto/provider/JctProviderTest.java
@@ -0,0 +1,17 @@
+package dev.scheibelhofer.crypto.provider;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class JctProviderTest {
+    
+    @Test
+    public void testGetInstnace() throws Exception {
+        JctProvider prov1 = JctProvider.getInstance();
+        assertNotNull(prov1);
+        JctProvider prov2 = JctProvider.getInstance();
+        assertTrue(prov1 == prov2);
+    }
+}
